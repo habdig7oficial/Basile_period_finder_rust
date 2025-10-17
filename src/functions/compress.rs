c862804abb0801c24f5d015862d061ec032ace1e
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-10-17 17:55:18 -0300

    ok

--- src/functions/compress.rs
+++ src/functions/compress.rs
@@ -1,16 +1,17 @@
-fn find_period(text: &str) -> Option<(u32, &str)> {
+fn find_period(text: &str) -> Option<(usize, &str)> {
     for i in 1..(text.len() / 2) {
         if text[0..i] == text[i..(i*2)] {
             //print!("{i} {0} - {1} ", &text[0..i], &text[i..(i*2)]);
             print!("true");
             let mut j = 0;
-            print!("bg");
-            while ((i*j) * 2) < text.len() {
-                println!("{j} {0}", &text[(i*j)..((i*j)*2)]);
-                j += 1
-            }
-            print!("end");
+            let mut counter = i; // The first element is already counted
+            while j < text.len() {
+                println!("{j} {0}", &text[j..(j + i)]);
 
+                counter += 1;
+                j += i
+            }
+            return Some((i, &text[0..i])); 
         }else {
             //print!("false");
         }
@@ -21,9 +22,6 @@ fn find_period(text: &str) -> Option<(u32, &str)> {
 
 pub fn new(text: &str){
     let mut i = 0;
-    while i < text.len() {
-        find_period(&text[i..]);
-        print!("hello");
-        i += 1;
-    }
+    find_period(&text[i..]);
+    
 }
