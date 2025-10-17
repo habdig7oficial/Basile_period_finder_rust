e143f55d1617f78b19a3f89bdb937457cbe2c4c4
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-10-17 18:55:04 -0300

    ok

--- src/functions/compress.rs
+++ src/functions/compress.rs
@@ -1,27 +1,41 @@
-fn find_period(text: &str) -> Option<(usize, &str)> {
+
+fn find_period(text: &str) -> (usize, usize, &str) {
     for i in 1..(text.len() / 2) {
         if text[0..i] == text[i..(i*2)] {
             //print!("{i} {0} - {1} ", &text[0..i], &text[i..(i*2)]);
-            print!("true");
+            //print!("true");
             let mut j = 0;
-            let mut counter = i; // The first element is already counted
-            while j < text.len() {
-                println!("{j} {0}", &text[j..(j + i)]);
+            let mut counter = 0;
 
+            while j + i < text.len() {
+                //println!("{j} {0}", &text[j..(j + i)]);
+                if text[0..i] != text[j..(j + i)] {
+                    break;
+                }
                 counter += 1;
                 j += i
             }
-            return Some((i, &text[0..i])); 
-        }else {
-            //print!("false");
+            return (counter, i, &text[0..i]); 
         }
         //println!();
     }
-    None
+    return (1, 1, &text); 
 }
 
 pub fn new(text: &str){
     let mut i = 0;
-    find_period(&text[i..]);
+
+    let mut string_builder = String::from("");
+
+    while i < text.len(){
+        let (counter, buffer_size, str) = find_period(&text[i..]);
+        println!("{str} ");
+        string_builder = format!("{string_builder}-{str}{counter}");  
+        i += counter * buffer_size;
+    }
+
+    string_builder = string_builder[1..].to_string(); 
+
+    print!("{string_builder}");
     
 }
